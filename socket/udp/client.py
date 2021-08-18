bd2460c6f86b3d71e18d839ea05c768b8a5258ee
HexChap <83132424+HexChap@users.noreply.github.com>
2021-08-18 16:05:06 +0300

    Delete socket/udp directory

--- socket/udp/client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-for i in range(100):
-    msg = f"{i+1}.Hello World!"
-    sock.sendto(bytes(msg, 'utf-8'), ("127.0.0.1", 8888))
